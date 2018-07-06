81d527fa668fbc56e9abf0dfce63ef707c0ed7f8
Evocal <cxevocal@gmail.com>
2018-07-06 03:19:04 +0200

    Texto de pruebo
    Añado 3 linea para ver la funcionalidad de esta cosa

--- bot.js
+++ bot.js
@@ -1,7 +1,9 @@
 var Discord = require('discord.js'); //Necesario para usar API de discord
 var bot = new Discord.Client(); //Necesario escucha cliente.
 
-
+//Esta linea de texto solo sirve para ver como funciona el GitHubDesktop
+//Se supone que al guardar me saldra una cosa verde bonita
+//O no
 
 var fs = require("fs"); //Lectura de ficheros
 var data; // Lectura JSON
